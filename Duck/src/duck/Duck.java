78a8f8b844e18ad194cc2f7b51ea76f5248f75e2
Stepan88 <9206740604@mail.ru>
2015-09-05 15:47:34 +0400

    Duck

---
+++ Duck/src/duck/Duck.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duck;
+
+/**
+ *
+ * @author Stud_17
+ */
+public class Duck {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
