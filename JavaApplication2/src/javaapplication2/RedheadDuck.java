351903a48f71997ca9eebcfa85b1ae656e30d301
unknown <Stud_17@17_14.efdnew.isuct.ru>
2015-09-05 16:30:33 +0400

    Duck

---
+++ JavaApplication2/src/javaapplication2/RedheadDuck.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Stud_17
+ */
+public class RedheadDuck {
+    
+}
