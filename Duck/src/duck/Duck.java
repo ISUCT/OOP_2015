a298f359bc09d38ee5da5a418bec083437e47100
dmitryal37 <dmitryal37@gmail.com>
2015-09-07 05:39:20 +0300

    Duck

---
+++ Duck/src/duck/Duck.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duck;
+
+/**
+ *
+ * @author dmitryal37
+ */
+public abstract class Duck {
+    
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    
+    public Duck() {
+   
+}
+
+    public abstract void display();
+    
+    public void performFly() {
+        flyBehavior.fly();
+    }
+    
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+    
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+}
