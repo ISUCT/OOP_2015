ccdccb0838ad0af70513f1b309bb3b931a211b09
Your Name <you@example.com>
2015-09-21 15:02:55 +0400

    test

--- OOP_2015Tsareva/test/oop_2015tsareva/MallardDuckTest.java
+++ OOP_2015Tsareva/test/oop_2015tsareva/MallardDuckTest.java
@@ -1,7 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
 package oop_2015tsareva;
 
 import java.io.ByteArrayOutputStream;
@@ -14,55 +14,63 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
- * @author stud_17
- */
+*
+* @author stud_17
+*/
 public class MallardDuckTest {
-    
-    public MallardDuckTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of display method, of class MallardDuck.
-     */
-    @Test
-    public void testDisplay() {
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(baos);
-        System.setOut(ps);
-        Duck mallard = new MallardDuck();
-        
-        mallard.performQuack();
-        assertEquals("Quack!\n", baos.toString());
-        baos.reset();
-        
-        
-        System.setOut(ps);
-        Duck readhead = new RedheadDuck();
-        
-        readhead.performFly();
-        assertEquals("I'm flying!\n", baos.toString());
-        
-        Duck model = new ModelDuck();
-        System.setOut(ps);
-        
-        model.performQuack();
-        assertEquals("Quack!\n", baos.toString());
-    }
+public MallardDuckTest() {
 }
+ByteArrayOutputStream baos = new ByteArrayOutputStream();
+PrintStream ps = new PrintStream(baos);
+
+@BeforeClass
+public static void setUpClass() {
+}
+
+@AfterClass
+public static void tearDownClass() {
+}
+
+@Before
+public void setUp() {
+System.setOut(ps);
+}
+
+@After
+public void tearDown() {
+baos.reset();
+}
+
+/**
+* Test of display method, of class MallardDuck.
+*/
+@Test
+public void testMallardQuack() {
+Duck mallard = new MallardDuck();
+mallard.performQuack();
+assertEquals("Quack!!!\r\n", baos.toString());
+}
+
+/**
+*
+*/
+@Test
+public void testRedheadDuck() {
+Duck readhead = new RedheadDuck();
+readhead.performFly();
+assertEquals("I'm flying!!\r\n", baos.toString());
+}
+
+/**
+*
+*/
+@Test
+public void testModelDuck() {
+Duck model = new ModelDuck();
+model.performQuack();
+assertEquals("Quack!!!\r\n", baos.toString());
+}
+}
+	
+
