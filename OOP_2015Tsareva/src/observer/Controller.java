f34d3d060690c47d6e685925704380c28f9a2dcc
unknown <stud_6@6_04.efdnew.isuct.ru>
2015-10-14 11:14:39 +0400

    controller

---
+++ OOP_2015Tsareva/src/observer/Controller.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package observer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author stud_6
+ */
+public class Controller implements Initializable {
+
+    @FXML
+    private Button myButton;
+
+   
+    public void initialize(URL location, ResourceBundle resources) {
+        assert myButton != null : "fx:id was not injected";
+
+         myButton.setOnAction(new EventHandler<ActionEvent>() {
+             @Override
+             public void handle(ActionEvent event) {
+                 System.out.println("Hello World");
+             }
+         
+         });
+
+
+    }
+}
