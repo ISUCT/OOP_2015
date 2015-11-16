0e92376c21c34bdf6a2a550b4107e30e67dd4c69
unknown <stud_17@17_01.efdnew.isuct.ru>
2015-11-16 14:50:03 +0400

    hghg

---
+++ OOP_2015_Orlova_E_A/src/Decorator/CondimentDecorator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Decorator;
+
+/**
+ *
+ * @author stud_17
+ */
+public abstract  class CondimentDecorator  extends Beverage {
+    public abstract String getDescription();
+    
+}
---
+++ OOP_2015_Orlova_E_A/src/Decorator/Espresso.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Decorator;
+
+/**
+ *
+ * @author stud_17
+ */
+public class Espresso e {
+    
+}
